24e0a6e640bf093e9d23c03ab7ca743f694ae58b
Maksim <erominmaksim@gmail.com>
2022-04-28 20:57:11 +0300

    add_step-3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $(".topnav-down").click(function() {
         $("html, body").animate({
-            scrollTop: $(".step-2-left").offset().top
+            scrollTop: $("#step-2").offset().top
         }, 1000);
     });
 });
